e602ca3d1f1049f7981a54b2c95818e5da1c79ba
iste19 <91463209+iste19@users.noreply.github.com>
2022-08-11 17:52:19 +1200

    Update mac_config.sh

--- mac_config.sh
+++ mac_config.sh
@@ -1,3 +1,4 @@
 curl https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.0.5-stable.zip --output flutter_macos_3.0.5-stable.zip
 unzip ./flutter_macos_3.0.5-stable.zip
 export PATH="$PATH:`pwd`/flutter/bin"
+chmod +x ./mac_config.sh
